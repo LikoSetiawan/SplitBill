d52d2c456c1798fe011649ab0350b24f6708527e
LikoSetiawan <128354782+LikoSetiawan@users.noreply.github.com>
2024-01-28 14:28:44 +0800

    Initial commit

---
+++ SplitBill/SplitBillApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplitBillApp.swift
+//  SplitBill
+//
+//  Created by Liko Setiawan on 28/01/24.
+//
+
+import SwiftUI
+
+@main
+struct SplitBillApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
